916b05b0d186b3fe3d9872fd4ac6f34014a75751
ddock4you <ddock4you@gmail.com>
2020-01-07 18:52:41 +0900

    - eslint 설치, eslint-plugin-react-hooks 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+    env: {
+        browser: true,
+        es6: true,
+        node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
     },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: "module"
     },
-    "plugins": [
-        "react"
+    plugins: [
+        "react",
+        ("react-hooks/rules-of-hooks": "error"), // Checks rules of Hooks
+        ("react-hooks/exhaustive-deps": "warn") // Checks effect dependencies
     ],
-    "rules": {
-    }
+    rules: {}
 };
